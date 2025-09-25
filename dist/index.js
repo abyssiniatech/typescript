e3829b7d012bfbec086431a8739dcba5dbe88734
surafel <surafelmengist2015@gmail.com>
2025-09-25 17:15:26 +0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = "surafel";
+function greet(name) {
+    return `hello ${name}`;
+}
+greet("surafel");
+// the second example
+let message = "Hello, TypeScript!";
+console.log(message);
+// Output: Hello, TypeScript!
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let name:string ="surafel";
+
+
+function greet(name:string){
+    return `hello ${name}`;
+}
+greet("surafel")
+
+
+
+
+// the second example
+let message: string = "Hello, TypeScript!";
+console.log(message);
+// Output: Hello, TypeScript!
