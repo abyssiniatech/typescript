68287179ec2845401f1ff9d1fe53ea4f277bab58
surafel <surafelmengist2015@gmail.com>
2025-09-27 13:24:06 +0300

    chick status inn ts

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,24 @@
 "use strict";
+// union in typscript 
+// union type in typescript is you set two probablity in typescript
+// example
 Object.defineProperty(exports, "__esModule", { value: true });
-const student = {
-    name: "surafel",
-    age: 23,
-    email: "sura@gmail.com"
-};
-function Student(student) {
-    return student;
+const id = 12;
+const name = "sura2015";
+// check the status in the function 
+function chickStataus(Status) {
+    if (Status === "success") {
+        return ("success");
+    }
+    else if (Status === "error") {
+        return "error";
+    }
+    else {
+        return "loading";
+    }
 }
-const result = Student(student);
+const statusval = chickStataus("error");
 console.log('====================================');
-console.log(` my name is ${result.name}`);
-console.log(` i 'm ${result.age} years old`);
-console.log(`my email addrese is ${result.email}`);
+console.log(statusval);
 console.log('====================================');
 //# sourceMappingURL=index.js.map
