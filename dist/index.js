5e8b9f468bee2464596ab67f7d3ad66b444f280b
surafel <surafelmengist2015@gmail.com>
2025-09-27 11:06:21 +0300

    lession-1

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let name = "surafel";
-function greet(name) {
-    return `hello ${name}`;
-}
-greet("surafel");
-// the second example
-let message = "Hello, TypeScript!";
+let message = "Hello TypeScript!";
 console.log(message);
-// Output: Hello, TypeScript!
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,2 @@
-let name:string ="surafel";
-
-
-function greet(name:string){
-    return `hello ${name}`;
-}
-greet("surafel")
-
-
-
-
-// the second example
-let message: string = "Hello, TypeScript!";
+let message: string = "Hello TypeScript!";
 console.log(message);
-// Output: Hello, TypeScript!
