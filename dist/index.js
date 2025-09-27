7e7fffcdd4ea300eabedba6692b90db937d4aa61
surafel <surafelmengist2015@gmail.com>
2025-09-27 11:12:35 +0300

    lession-2

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let message = "Hello TypeScript!";
-console.log(message);
+// types in typescrippt
+// there are three common types in typescript
+// 1 number type
+let age = 25;
+let price = 19.99;
+const pi = 3.14;
+console.log(age, price, pi);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-let message: string = "Hello TypeScript!";
-console.log(message);
+// types in typescrippt
+// there are three common types in typescript
+// 1 number type
+let age: number = 25;
+let price = 19.99;
+const pi :number = 3.14;
+console.log(age, price, pi);
