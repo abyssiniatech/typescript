1c57d5c2085a732379788543e60f70cac28e6cbd
surafel <surafelmengist2015@gmail.com>
2025-09-27 12:49:37 +0300

    function in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
-// any type in typescript
-// any type can hold any value and can be reassigned to different types
-let data:any = 42;
-data = "Hello, World!";
-data = true;
-data = { name: "Alice", age: 30 };
-data=null;
-data=undefined;
-data=data;
-
-
-
-
+// function  in typescript
+// function in typescript with type annotations
+function greet(name: string,age:number) {
+  return `Hello,  my name is  ${name} and i am ${age} years old`;
+}
+console.log(greet("Alice",12));
