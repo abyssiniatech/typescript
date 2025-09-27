4ec2bd068484ae0e2006b8796735e0e408b3d447
surafel <surafelmengist2015@gmail.com>
2025-09-27 12:06:04 +0300

    tupple in typescript

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "surafel",
-    age: 30,
-    email: "sura@gmail.com"
-};
-function information(person) {
-    return { name: person.name, age: person.age, email: person.email };
-}
-const val = information({ name: person.name, age: person.age, email: person.email });
-console.log(val);
+// tupples in ts
+const person = ["John", 30, true];
+console.log(person);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,4 @@
-//object types in ts
-// declare types
-type informationProps = {
-    name: string,
-    age: number,
-    email: string
-}
+// tupples in ts
+const person :[string, number, boolean]=["John", 30, true];
 
-
-const person: informationProps = {
-    name: "surafel",
-    age: 30,
-    email: "sura@gmail.com"
-}
-
-
-
-
-function information(person: informationProps) {
-    return { name: person.name, age: person.age, email: person.email };
-}
-
-const val = information({ name: person.name, age: person.age, email: person.email });
-console.log(val);
-
-
-
-
-export {};
+console.log(person);
